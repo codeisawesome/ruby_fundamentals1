8eb9e9c838bd359b79201e7fb3022dfe9311a6e2
Rui Dong <rui.dong.jh@gmail.com>
2016-08-09 09:57:09 -0400

    this program puts Bit, Maker, or Bitmaker depending on the remainder

--- exercise3.rb
+++ exercise3.rb
@@ -1,5 +1,5 @@
 puts "What is your name?"
 
-name = gets.chomp
+name = gets.chomp.to_s
 
 puts "Hi #{name}!"
---
+++ exercise4.rb
@@ -0,0 +1,14 @@
+i = 1
+
+while i < 100
+  if i % 15 == 0
+    puts "BitMaker"
+  elsif i % 3 == 0
+    puts "Bit"
+  elsif i % 5 == 0
+    puts "Maker"
+  else
+    puts i
+  end
+  i += 1
+end
